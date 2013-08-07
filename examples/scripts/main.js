0bddc45c41f887d8bd3a17cb953004f063b72f9f
Sahat Yalkabov <sakhat@gmail.com>
2013-08-07 15:03:59 -0500

    Coffeed it up!

--- examples/scripts/main.js
+++ examples/scripts/main.js
@@ -2,10 +2,12 @@ require.config({
   paths: {
     "jquery": "vendor/jquery/jquery",
     "underscore": "vendor/underscore-amd/underscore",
-    "backbone": "vendor/backbone-amd/backbone"
+    "backbone": "vendor/backbone-amd/backbone",
+    "cs": "vendor/require-cs/cs",
+    "coffee-script": "vendor/coffee-script/extras/coffee-script"
   }
 });
 
-require(['views/app'], function(AppView) {
+require(['cs!views/app'], function(AppView) {
   new AppView;
 });
---
+++ examples/scripts/views/app.coffee
@@ -0,0 +1,6 @@
+define ["backbone"], (Backbone) ->
+  App = Backbone.View.extend
+    initialize: ->
+    console.log "Hello from continuum analytics!"
+
+  App
